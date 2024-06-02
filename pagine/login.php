59c5cb50d5a73d17aa37503d0e0b137aa8208656
ma2rt2y <06.marty.v@gmail.com>
2024-06-02 12:53:33 +0200

    marty

--- pagine/login.php
+++ pagine/login.php
@@ -17,40 +17,43 @@
         require("nav.php");
     ?>    
 
-    <div class="contenuto">
-        <form action="" method="post">
-            <table class="tabella">
-                <tr>
-                    <td><label for="username">Username :</label></td>
-                    <td><input type="text" name="username" id="username" value= "<?php echo $username ?>" required></td>
-                </tr>
-                <tr>
-                    <td><label for="password">Password :</label></td>
-                    <td><input type="password" name="password" id="password" required></td>
-                </tr>
-            </table>
-        </form>
+    <div class="contenuto-background">
+        <div class="contenuto">
+            <form action="" method="post">
+                <table class="tabella">
+                    <tr>
+                        <td><label for="username" class="medium-text cw">Username :</label></td>
+                        <td><input type="text" name="username" id="username" value= "<?php echo $username ?>" required></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password" class="medium-text cw">Password :</label></td>
+                        <td><input type="password" name="password" id="password" required></td>
+                    </tr>
+                </table>
+                <div class="button-container">
+                    <input type="submit" class="button cw" value="Accedi">
+                </div>
+            </form>
 
-        <input type="submit" class="button" value="accedi">
-
-        <?php
-            if(isset($_POST['password']) and isset($_POST['username'])){
-                require("../data/connessione_db.php");
-                $sql="SELECT username, password 
-                    FROM utente
-                    WHERE username='$username' and password='$password'";
-                $ris=$conn->query($sql) or die("<p>Query fallita</p>");
-                if($ris->num_rows==0){
-                    echo "<p>username o password non trovati</p>";
-                }
-                else{
-                    session_start();
-                    $_SESSION['username']=$username;
-                    $conn->close();
-                    header("pianeti.php");
-                }
-            }    
-        ?>
+            <?php
+                if(isset($_POST['password']) and isset($_POST['username'])){
+                    require("../data/connessione_db.php");
+                    $sql="SELECT username, password 
+                        FROM utente
+                        WHERE username='$username' and password='$password'";
+                    $ris=$conn->query($sql) or die("<p>Query fallita</p>");
+                    if($ris->num_rows==0){
+                        echo "<p>username o password non trovati</p>";
+                    }
+                    else{
+                        session_start();
+                        $_SESSION['username']=$username;
+                        $conn->close();
+                        header("pianeti.php");
+                    }
+                }    
+            ?>
+        </div>
     </div>
     
 </body>
