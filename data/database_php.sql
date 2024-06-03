5cfb820436132c67aa39face13760525c6238d97
gaia <gaiafedeli2@gmail.com>
2024-06-03 13:14:34 +0200

    database

--- data/database_php.sql
+++ data/database_php.sql
@@ -15,62 +15,58 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for database_php
-CREATE DATABASE IF NOT EXISTS `database_php` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `database_php`;
+-- Dumping database structure for database_php1
+CREATE DATABASE IF NOT EXISTS `database_php1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `database_php1`;
 
--- Dumping structure for table database_php.navicella
+-- Dumping structure for table database_php1.navicella
 CREATE TABLE IF NOT EXISTS `navicella` (
-  `nome` char(50) DEFAULT NULL,
-  `codice` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_navicella` char(50) DEFAULT NULL,
+  `cod_navicella` char(50) NOT NULL,
   `username_utente` char(50) DEFAULT NULL,
-  PRIMARY KEY (`codice`),
+  PRIMARY KEY (`cod_navicella`),
   KEY `username_utente` (`username_utente`),
-  CONSTRAINT `FK1_navicella_utente` FOREIGN KEY (`username_utente`) REFERENCES `utenti` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table database_php.navicella: ~0 rows (approximately)
-REPLACE INTO `navicella` (`nome`, `codice`, `username_utente`) VALUES
-	('Supernova', 1, 'gaiafedeli');
-
--- Dumping structure for table database_php.pianeta
-CREATE TABLE IF NOT EXISTS `pianeta` (
-  `nome` char(50) DEFAULT NULL,
-  `codice` int(11) NOT NULL AUTO_INCREMENT,
-  `diametro` int(11) DEFAULT NULL,
-  PRIMARY KEY (`codice`)
+  CONSTRAINT `FK1_username_utente` FOREIGN KEY (`username_utente`) REFERENCES `utenti` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table database_php.pianeta: ~0 rows (approximately)
+-- Dumping data for table database_php1.navicella: ~0 rows (approximately)
 
--- Dumping structure for table database_php.utenti
+-- Dumping structure for table database_php1.pianeta
+CREATE TABLE IF NOT EXISTS `pianeta` (
+  `nome_pianeta` char(50) DEFAULT NULL,
+  `cod_pianeta` char(50) NOT NULL,
+  `diametro` int(14) DEFAULT NULL,
+  PRIMARY KEY (`cod_pianeta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table database_php1.pianeta: ~0 rows (approximately)
+
+-- Dumping structure for table database_php1.utenti
 CREATE TABLE IF NOT EXISTS `utenti` (
-  `nome` char(50) DEFAULT NULL,
+  `nome_utente` char(50) DEFAULT NULL,
   `cognome` char(50) DEFAULT NULL,
   `username` char(50) NOT NULL,
+  `password` char(50) NOT NULL,
   `email` char(50) DEFAULT NULL,
   `telefono` int(11) DEFAULT NULL,
   `comune` char(50) DEFAULT NULL,
   `indirizzo` char(50) DEFAULT NULL,
-  `password` char(50) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table database_php.utenti: ~1 rows (approximately)
-REPLACE INTO `utenti` (`nome`, `cognome`, `username`, `email`, `telefono`, `comune`, `indirizzo`, `password`) VALUES
-	('Gaia', 'Fedeli', 'gaiafedeli', 'gaiafedeli2@gmail.com', NULL, 'Caponago', NULL, '');
+-- Dumping data for table database_php1.utenti: ~0 rows (approximately)
 
--- Dumping structure for table database_php.viaggia
+-- Dumping structure for table database_php1.viaggia
 CREATE TABLE IF NOT EXISTS `viaggia` (
-  `codice_navicella` int(11) NOT NULL DEFAULT 0,
-  `codice_pianeta` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`codice_navicella`,`codice_pianeta`),
-  KEY `FK2_codice_pianeta` (`codice_pianeta`),
-  CONSTRAINT `FK1_codice_navicella` FOREIGN KEY (`codice_navicella`) REFERENCES `navicella` (`codice`),
-  CONSTRAINT `FK2_codice_pianeta` FOREIGN KEY (`codice_pianeta`) REFERENCES `pianeta` (`codice`)
+  `cod_navicella` char(50) NOT NULL,
+  `cod_pianeta` char(50) NOT NULL,
+  PRIMARY KEY (`cod_navicella`,`cod_pianeta`) USING BTREE,
+  KEY `FK2_cod_pianeta` (`cod_pianeta`),
+  CONSTRAINT `FK1_cod_navicella` FOREIGN KEY (`cod_navicella`) REFERENCES `navicella` (`cod_navicella`),
+  CONSTRAINT `FK2_cod_pianeta` FOREIGN KEY (`cod_pianeta`) REFERENCES `pianeta` (`cod_pianeta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table database_php.viaggia: ~0 rows (approximately)
+-- Dumping data for table database_php1.viaggia: ~0 rows (approximately)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
