ba3adf05a9b314435fe57f0255ca9c0e4119e527
gaia <gaiafedeli2@gmail.com>
2024-06-04 11:17:26 +0200

    ritira e data

--- data/database_php.sql
+++ data/database_php.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versione server:              10.4.28-MariaDB - mariadb.org binary distribution
--- S.O. server:                  Win64
--- HeidiSQL Versione:            12.5.0.6677
+-- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.5.0.6677
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -15,74 +15,78 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dump della struttura del database database_php
+-- Dumping database structure for database_php
 CREATE DATABASE IF NOT EXISTS `database_php` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `database_php`;
 
--- Dump della struttura di tabella database_php.navicella
+-- Dumping structure for table database_php.navicella
 CREATE TABLE IF NOT EXISTS `navicella` (
   `nome_navicella` char(50) DEFAULT NULL,
   `cod_navicella` char(50) NOT NULL,
   `username_utente` char(50) DEFAULT NULL,
+  `prezzo` int(11) DEFAULT NULL,
   PRIMARY KEY (`cod_navicella`),
   KEY `username_utente` (`username_utente`),
   CONSTRAINT `FK1_username_utente` FOREIGN KEY (`username_utente`) REFERENCES `utenti` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dump dei dati della tabella database_php.navicella: ~40 rows (circa)
-REPLACE INTO `navicella` (`nome_navicella`, `cod_navicella`, `username_utente`) VALUES
-	('SS Vulture', '12469785', NULL),
-	('Ripper', '12576354', NULL),
-	('P-8 Albatross', '16479855', NULL),
-	('Huntress', '19528943', NULL),
-	('STS Alice', '19785957', NULL),
-	('USS Exploration', '23456877', NULL),
-	('BS Invincible', '25489673', NULL),
-	('SC Fighter', '31245875', NULL),
-	('Violet Panama', '32556256', NULL),
-	('Stellar Flare', '34798512', NULL),
-	('The Kobayashi', '35489568', NULL),
-	('CS Carnage V', '45231845', NULL),
-	('BS Vespira', '46798524', NULL),
-	('LWSS The Jellyfish', '51873642', NULL),
-	('Divine Intervention', '54687129', NULL),
-	('Duke Elba', '56232658', NULL),
-	('Pursuer', '56723841', NULL),
-	('Elizabeth', '63457528', NULL),
-	('Akimbo Maze', '64758238', NULL),
-	('Fuchsia Midway', '65485128', NULL),
-	('HWSS Spectrum', '65496512', NULL),
-	('Galactic Core', '65783918', NULL),
-	('BC Zion', '66245879', NULL),
-	('Hummingbird', '71395256', NULL),
-	('SC Cataphract', '74569167', NULL),
-	('STS Deinonychus', '75395146', NULL),
-	('Dark Phoenix', '76458311', NULL),
-	('Lime Victory', '78545985', NULL),
-	('K-26 Matador', '78987653', NULL),
-	('STS Analyzer', '81346795', NULL),
-	('BS Empress', '85214653', NULL),
-	('Ravana Mark IV', '85445689', NULL),
-	('Battlecruiser Elena', '85631599', NULL),
-	('HMS Atlas', '86455732', NULL),
-	('Halcyon Invincible 3', '86475365', NULL),
-	('D2 Stalwart II', '87129621', NULL),
-	('Harmony', '96458731', NULL),
-	('Banger Kryptoria', '96476535', NULL),
-	('STS Athens IV', '96522542', NULL),
-	('BS Independence', '98563265', NULL);
+-- Dumping data for table database_php.navicella: ~40 rows (approximately)
+REPLACE INTO `navicella` (`nome_navicella`, `cod_navicella`, `username_utente`, `prezzo`) VALUES
+	('SS Vulture', '12469785', NULL, 12300000),
+	('Ripper', '12576354', NULL, 9930000),
+	('P-8 Albatross', '16479855', NULL, 1300000),
+	('Huntress', '19528943', NULL, 1200000),
+	('STS Alice', '19785957', NULL, 2300000),
+	('USS Exploration', '23456877', NULL, 8930000),
+	('BS Invincible', '25489673', NULL, 7830000),
+	('SC Fighter', '31245875', NULL, 6960000),
+	('Violet Panama', '32556256', NULL, 3830000),
+	('Stellar Flare', '34798512', NULL, 4570000),
+	('The Kobayashi', '35489568', NULL, 84300000),
+	('CS Carnage V', '45231845', NULL, 56000700),
+	('BS Vespira', '46798524', NULL, 49000000),
+	('LWSS The Jellyfish', '51873642', NULL, 20000000),
+	('Divine Intervention', '54687129', NULL, 40000000),
+	('Duke Elba', '56232658', NULL, 70000000),
+	('Pursuer', '56723841', NULL, 999999),
+	('Elizabeth', '63457528', NULL, 54300000),
+	('Akimbo Maze', '64758238', NULL, 2430400),
+	('Fuchsia Midway', '65485128', NULL, 56470000),
+	('HWSS Spectrum', '65496512', NULL, 19700000),
+	('Galactic Core', '65783918', NULL, 32800000),
+	('BC Zion', '66245879', NULL, 3726000),
+	('Hummingbird', '71395256', NULL, 34272900),
+	('SC Cataphract', '74569167', NULL, 8363500),
+	('STS Deinonychus', '75395146', NULL, 26243900),
+	('Dark Phoenix', '76458311', NULL, 32835000),
+	('Lime Victory', '78545985', NULL, 8264800),
+	('K-26 Matador', '78987653', NULL, 26284500),
+	('STS Analyzer', '81346795', NULL, 5532900),
+	('BS Empress', '85214653', NULL, 73538400),
+	('Ravana Mark IV', '85445689', NULL, 72544380),
+	('Battlecruiser Elena', '85631599', NULL, 7543200),
+	('HMS Atlas', '86455732', NULL, 85320000),
+	('Halcyon Invincible 3', '86475365', NULL, 6243000),
+	('D2 Stalwart II', '87129621', NULL, 8424000),
+	('Harmony', '96458731', NULL, 93547000),
+	('Banger Kryptoria', '96476535', NULL, 64332200),
+	('STS Athens IV', '96522542', NULL, 76452300),
+	('BS Independence', '98563265', NULL, 34606900);
 
--- Dump della struttura di tabella database_php.pianeta
+-- Dumping structure for table database_php.pianeta
 CREATE TABLE IF NOT EXISTS `pianeta` (
   `nome_pianeta` char(50) DEFAULT NULL,
   `cod_pianeta` char(50) NOT NULL,
   `diametro` int(14) DEFAULT NULL,
-  PRIMARY KEY (`cod_pianeta`)
+  `cod_navicella` char(50) DEFAULT NULL,
+  PRIMARY KEY (`cod_pianeta`),
+  KEY `cod_navicella` (`cod_navicella`),
+  CONSTRAINT `FK1_cod_navicella` FOREIGN KEY (`cod_navicella`) REFERENCES `navicella` (`cod_navicella`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dump dei dati della tabella database_php.pianeta: ~0 rows (circa)
+-- Dumping data for table database_php.pianeta: ~0 rows (approximately)
 
--- Dump della struttura di tabella database_php.utenti
+-- Dumping structure for table database_php.utenti
 CREATE TABLE IF NOT EXISTS `utenti` (
   `username` char(50) NOT NULL,
   `nome_utente` char(50) DEFAULT NULL,
@@ -95,24 +99,12 @@ CREATE TABLE IF NOT EXISTS `utenti` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dump dei dati della tabella database_php.utenti: ~4 rows (circa)
+-- Dumping data for table database_php.utenti: ~4 rows (approximately)
 REPLACE INTO `utenti` (`username`, `nome_utente`, `cognome`, `password`, `email`, `telefono`, `comune`, `indirizzo`) VALUES
 	('asd', 'asd', 'asd', 'asd', 'asd@gmail.com', 1234567891, 'Aicurzio', 'Via croce, 1'),
 	('gaiafedeli', 'Gaia', 'Fedeli', '123', 'gaia.fedeli@liceobanfi.eu', 2147483647, 'Caponago', 'Via Tua Madre, 18'),
 	('marti', 'Martina', 'Villa', 'aaaaaaaaaa', 'martina.villa@liceobanfi.eu', 2147483647, 'Agrate', 'Via Biagio, 72'),
 	('martina', 'Martina', 'Villa', 'aaaa', 'martina.villa@liceobanfi.eu', 2147483647, 'Agrate', 'Via Biagio, 72');
-
--- Dump della struttura di tabella database_php.viaggia
-CREATE TABLE IF NOT EXISTS `viaggia` (
-  `cod_navicella` char(50) NOT NULL,
-  `cod_pianeta` char(50) NOT NULL,
-  PRIMARY KEY (`cod_navicella`,`cod_pianeta`) USING BTREE,
-  KEY `FK2_cod_pianeta` (`cod_pianeta`),
-  CONSTRAINT `FK1_cod_navicella` FOREIGN KEY (`cod_navicella`) REFERENCES `navicella` (`cod_navicella`),
-  CONSTRAINT `FK2_cod_pianeta` FOREIGN KEY (`cod_pianeta`) REFERENCES `pianeta` (`cod_pianeta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dump dei dati della tabella database_php.viaggia: ~0 rows (circa)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
