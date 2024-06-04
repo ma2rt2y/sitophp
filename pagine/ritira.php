7406fb99944c21bc611433b499db94bb16a220cf
ma2rt2y <06.marty.v@gmail.com>
2024-06-04 14:38:39 +0200

    marty

--- pagine/ritira.php
+++ pagine/ritira.php
@@ -49,34 +49,34 @@
     </div>
 
     <form action="" method = "post">
-            <?php
-                if(isset($_POST["nome_navicella"]) and isset($_POST["nome_pianeta"]) and isset($_POST["prezzo"])){
-                    $nome_navicella = $_POST["nome_navicella"];
-                    $nome_pianeta = $_POST["nome_pianeta"];
-                    $prezzo = $_POST["prezzo"];
+        <?php
+            if(isset($_POST["nome_navicella"]) and isset($_POST["nome_pianeta"]) and isset($_POST["prezzo"])){
+                $nome_navicella = $_POST["nome_navicella"];
+                $nome_pianeta = $_POST["nome_pianeta"];
+                $prezzo = $_POST["prezzo"];
 
-                    $sql = "SELECT navicella.nome_navicella, pianeta.nome_pianeta, navicella.prezzo
-                            FROM pianeta JOIN navicella ON pianeta.cod_navicella = navicella.cod_navicella
-                                            JOIN utenti ON navicella.username_utente = utenti.username
-                            WHERE nome_navicella LIKE '%$nome_navicella%'
-                                AND nome_pianeta LIKE '%$nome_pianeta%'
-                                AND prezzo LIKE '%$prezzo%'";
+                $sql = "SELECT navicella.nome_navicella, pianeta.nome_pianeta, navicella.prezzo
+                        FROM pianeta JOIN navicella ON pianeta.cod_navicella = navicella.cod_navicella
+                                        JOIN utenti ON navicella.username_utente = utenti.username
+                        WHERE nome_navicella LIKE '%$nome_navicella%'
+                            AND nome_pianeta LIKE '%$nome_pianeta%'
+                            AND prezzo LIKE '%$prezzo%'";
+                
+                $ris = $conn->query($sql) or die("<p>Query fallita!</p>");
+                if ($ris->num_rows > 0) {
+                    echo "<p>Scegli tra le soluzioni trovate la navicela che preferisci.</p>";
                     
-                    $ris = $conn->query($sql) or die("<p>Query fallita!</p>");
-                    if ($ris->num_rows > 0) {
-                        echo "<p>Scegli tra le soluzioni trovate la navicela che preferisci.</p>";
-                        
-                        foreach($ris as $riga){
-                            $cod_navicella = $riga["cod_navicella"];
-                            $nome_navicella = $riga["nome_navicella"];
-                            $img = $riga["img_navicella"];
-                            $prezzo = $riga["prezzo"];
-                            $nome_pianeta = $riga["nome_pianeta"];
+                    foreach($ris as $riga){
+                        $cod_navicella = $riga["cod_navicella"];
+                        $nome_navicella = $riga["nome_navicella"];
+                        $img = $riga["img_navicella"];
+                        $prezzo = $riga["prezzo"];
+                        $nome_pianeta = $riga["nome_pianeta"];
 
-                            echo <<<EOD 
-                                <div class="elenco_navicelle">
-                                <div class="card-navicelle">
-                                    <div class="card-navicelle__img">
+                        echo <<<EOD
+                        <div class="elenco_navicelle">
+                            <div class="card-navicelle">
+                                <div class="card-navicelle__img">
                                         <img src="../media/immagini/$img" alt="$img">
                                     </div>
                                     <div class="card-navicelle__testo">
@@ -84,34 +84,35 @@
                                             <p>Nome navicella: $nome_navicella</p>
                                             <p>Prezzo: $prezzo</p>
                                             <p>Nome pianeta: $nome_pianeta</p>
-                                            <p class="link-scheda"><a href="scheda-navicella.php?cod_navicella=$cod_navicella">Scheda navicella</a></p>        
-                            EOD;
-                            if($riga["username_utente"]) {
-                                echo "         <p>Disponibile: No</p>";
-                            }
-                            else {
-                                echo "         <p>Disponibile: Sì</p>"
-                                echo "         <p><input type='checkbox' name='cod_navicelle[]' value='$cod_navicella'/>Spunta per prenotare la tua navicella</p>";
-                            }
-                            echo <<<EOD 
-                                            </div>
+                                            <p class="link-scheda"><a href="scheda-navicella.php?cod_navicella=$cod_navicella">Scheda navicella</a></p>
+                        EOD; 
+                        if ($riga["username_utente"]){
+                            echo "          <p>Disponibile: No</p>";
+                        }
+                        else {
+                            echo "          <p>Disponibile: Sì</p>";
+                            echo "          <p><input type='checkbox' name='cod_navicelle[]' value='$cod_navicella'/> Spunta per prendere il libro</p>";
+                        }
+                        echo <<<EOD
                                         </div>
                                     </div>
                                 </div>
-                            EOD;
-                        }
-                        echo <<<EOD
-                            "<div class="button-container">
-                                <input type="submit" class="button1 cw small-text" value="Conferma">
-                            </div>"
+                            </div>
                         EOD;
                     }
-                    else{
-                        echo "<p>Non ho trovato alcuna navicella che rispetti le sue richieste</p>"
-                    }
-                    echo "</table>";
+                    echo <<<EOD
+                        "<div class="button-container">
+                            <input type="submit" class="button1 cw small-text" value="Conferma">
+                        </div>"
+                    EOD;
                 }
-            ?>
+                else {
+                    echo "<p>Non ho trovato alcuna navicella che rispetti le sue richieste</p>";
+                }
+                echo "</table>";
+            }
+
+        ?>
     </form>
     <?php
         require('footer1.php');
